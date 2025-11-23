cac39688daa9ad8fede418e1d9cf49e788282346
Stefano Famà <8192713+famastefano@users.noreply.github.com>
2025-11-23 14:19:55 +0100

    Added presets with toolchain

---
+++ cmake/llvm-win32-toolchain.cmake
@@ -0,0 +1,18 @@
+if(NOT WIN32)
+    return()
+endif()
+
+set(CMAKE_C_COMPILER "clang.exe")
+set(CMAKE_CXX_COMPILER "clang++.exe")
+
+list(APPEND CMAKE_INCLUDE_PATH
+    "C:/Program Files (x86)/Windows Kits/10/Include/10.0.26100.0/ucrt"
+    "C:/Program Files (x86)/Windows Kits/10/Include/10.0.26100.0/um"
+    "C:/Program Files (x86)/Windows Kits/10/Include/10.0.26100.0/shared"
+    "C:/Program Files/Microsoft Visual Studio/18/Community/VC/Tools/MSVC/14.50.35717/include"
+)
+link_directories(
+    "C:/Program Files (x86)/Windows Kits/10/Lib/10.0.26100.0/ucrt/x64"
+    "C:/Program Files (x86)/Windows Kits/10/Lib/10.0.26100.0/um/x64"
+    "C:/Program Files/Microsoft Visual Studio/18/Community/VC/Tools/MSVC/14.50.35717/lib/x64"
+)
